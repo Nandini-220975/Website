837693600cc68e0df5a863d5bd2c35ff37c191bb
Nandini-220975 <n220975@rguktn.ac.in>
2026-02-17 18:32:10 +1000

    setup

---
+++ lab2/php/do.php
@@ -0,0 +1,18 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#avail 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBDJylYgJ4R8cV1yxJHq-0u9UUaKGEZ0d4",
+    authDomain: "myproject28-bdc6a.firebaseapp.com",
+    projectId: "myproject28-bdc6a",
+    storageBucket: "myproject28-bdc6a.firebasestorage.app",
+    messagingSenderId: "871010400659",
+    appId: "1:871010400659:web:64554f89972414c3719066"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
